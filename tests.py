4c5df8c5cb904361490ef5474b19cd1690ef069b
Linus Östberg <linus@talavis.eu>
2016-01-27 14:23:21 +0100

    added default test

--- tests.py
+++ tests.py
@@ -12,3 +12,15 @@ def rcomplement(dna) :
 def test_rcomplement() :
     assert rcomplement('AACGGT') == 'ACCGTT'
     assert rcomplement('ACGTZ') == ''
+
+def get_word_lengths(s):
+    """
+    Returns a list of integers representing
+    the word lengths in string s.
+    """
+    return None
+
+
+def test_get_word_lengths():
+    text = "Three tomatoes are walking down the street"
+    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
